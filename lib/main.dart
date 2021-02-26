c4e2e534b5b4c36d2d118b2f9401f0a76809e16f
Debora <deborarebelatto1@gmail.com>
2021-02-26 11:26:51 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:bluetooth_thermal_printer/bluetooth_thermal_printer.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:flutter/services.dart';
+import 'package:image/image.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  bool connected = false;
+  List availableBluetoothDevices = new List();
+
+  Future<void> getBluetooth() async {
+    final List bluetooths = await BluetoothThermalPrinter.getBluetooths;
+    print("Print $bluetooths");
+    setState(() {
+      availableBluetoothDevices = bluetooths;
+    });
+  }
+
+  Future<void> setConnect(String mac) async {
+    final String result = await BluetoothThermalPrinter.connect(mac);
+    print("state conneected $result");
+    if (result == "true") {
+      setState(() {
+        connected = true;
+      });
+    }
+  }
+
+  Future<void> printTicket() async {
+    String isConnected = await BluetoothThermalPrinter.connectionStatus;
+    if (isConnected == "true") {
+      Ticket ticket = await getTicket();
+      final result = await BluetoothThermalPrinter.writeBytes(ticket.bytes);
+      print("Print $result");
+    } else {
+      //Hadnle Not Connected Senario
+    }
+  }
+
+  Future<void> printGraphics() async {
+    String isConnected = await BluetoothThermalPrinter.connectionStatus;
+    if (isConnected == "true") {
+      Ticket ticket = await getGraphicsTicket();
+      final result = await BluetoothThermalPrinter.writeBytes(ticket.bytes);
+      print("Print $result");
+    } else {
+      //Hadnle Not Connected Senario
+    }
+  }
+
+  Future<Ticket> getGraphicsTicket() async {
+    CapabilityProfile profile = await CapabilityProfile.load();
+    final Ticket ticket = Ticket(PaperSize.mm80, profile);
+
+    // Print QR Code using native function
+    ticket.qrcode('example.com');
+
+    ticket.hr();
+
+    // Print Barcode using native function
+    final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
+    ticket.barcode(Barcode.upcA(barData));
+
+    ticket.cut();
+
+    return ticket;
+  }
+
+  Future<Ticket> getTicket() async {
+    CapabilityProfile profile = await CapabilityProfile.load();
+    final Ticket ticket = Ticket(PaperSize.mm80, profile);
+
+
+
+    final ByteData dataRes = await rootBundle.load('assets/response.png');
+    final Uint8List bytesres = dataRes.buffer.asUint8List();
+    final Image imageres = decodeImage(bytesres);
+    ticket.image(imageres);
+
+    final ByteData dataBun = await rootBundle.load('assets/resizerab.jpg');
+    final Uint8List bytesbun = dataBun.buffer.asUint8List();
+    final Image imagebun = decodeImage(bytesbun);
+    ticket.image(imagebun);
+
+    ticket.text("Demo Shop",
+      styles: PosStyles(
+        align: PosAlign.center,
+        height: PosTextSize.size2,
+        width: PosTextSize.size2,
+      ),
+      linesAfter: 1
+    );
+
+    ticket.text(
+        "18th Main Road, 2nd Phase, J. P. Nagar, Bengaluru, Karnataka 560078",
+        styles: PosStyles(align: PosAlign.center));
+    ticket.text('Tel: +919591708470',
+        styles: PosStyles(align: PosAlign.center));
+
+
+    ticket.cut();
+    return ticket;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bluetooth Thermal Printer Demo'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Search Paired Bluetooth"),
+              OutlineButton(
+                onPressed: () {
+                  this.getBluetooth();
+                },
+                child: Text("Search"),
+              ),
+              Container(
+                height: 200,
+                child: ListView.builder(
+                  itemCount: availableBluetoothDevices.length > 0
+                      ? availableBluetoothDevices.length
+                      : 0,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () {
+                        String select = availableBluetoothDevices[index];
+                        List list = select.split("#");
+                        String name = list[0];
+                        String mac = list[1];
+                        this.setConnect(mac);
+                      },
+                      title: Text('${availableBluetoothDevices[index]}'),
+                      subtitle: Text("Click to connect"),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              OutlineButton(
+                onPressed: connected ? this.printGraphics : null,
+                child: Text("Print"),
+              ),
+              OutlineButton(
+                onPressed: connected ? this.printTicket : null,
+                child: Text("Print Ticket"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
